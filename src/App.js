f8a3bdc00d61b6fb58d406a38ff8dfb590455c3a
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2025-02-03 02:43:41 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Re
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
 import TaskEdit from "./components/TaskEdit";
 import Home from "./components/Home";
 
