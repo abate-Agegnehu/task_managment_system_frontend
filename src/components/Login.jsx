46267a1cfaa85f4c3e631e2e55ac99926a8d03b6
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2025-02-03 02:32:55 +0300

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(
+      const response = await axios.post(  
         "http://localhost:8888/api/auth/login",
         {
           email,
